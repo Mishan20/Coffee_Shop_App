a0e4b2851eaaeecd37a8ccfcf18fa4cee80689f7
Mishan20 <ishansenanayaka1@gmail.com>
2023-12-02 17:26:16 +0530

    created pages and did the navigation ech pages

--- App.tsx
+++ App.tsx
@@ -2,9 +2,9 @@ import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import HomeScreen from './src/screens/HomeScreen';
 import DetailsScreen from './src/screens/DetailsScreen';
 import PaymentScreen from './src/screens/PaymentScreen';
+import TabNavigator from './src/navigators/TabNavigator';
 
 const stack = createNativeStackNavigator();
 
@@ -13,13 +13,18 @@ const App = () => {
     <NavigationContainer>
       <stack.Navigator screenOptions={{headerShown: false}}>
         <stack.Screen
-          name="Details"
-          component={DetailsScreen}
+          name="Tab"
+          component={TabNavigator}
           options={{animation: 'slide_from_bottom'}}></stack.Screen>
 
         <stack.Screen
           name="Payment"
           component={PaymentScreen}
+          options={{animation: 'slide_from_bottom'}}></stack.Screen>
+
+        <stack.Screen
+          name="Details"
+          component={DetailsScreen}
           options={{animation: 'slide_from_bottom'}}></stack.Screen>
       </stack.Navigator>
     </NavigationContainer>
